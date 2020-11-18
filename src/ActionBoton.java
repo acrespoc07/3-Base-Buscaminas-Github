1211a93414a0dfd0b885e3e796e6610940afb00d
alex <alextorna99@gmailcom>
2020-11-18 13:17:41 +0100

    Cambios ActionBoton

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -1,61 +1,53 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
 /**
- * Clase que implementa el listener de los botones del Buscaminas.
- * De alguna manera tendrá que poder acceder a la ventana principal.
- * Se puede lograr pasando en el constructor la referencia a la ventana.
- * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
+ * Clase que implementa el listener de los botones del Buscaminas. De alguna
+ * manera tendrá que poder acceder a la ventana principal. Se puede lograr
+ * pasando en el constructor la referencia a la ventana. Recuerda que desde la
+ * ventana, se puede acceder a la variable de tipo ControlJuego
+ * 
  * @author jesusredondogarcia
  **
  */
-public class ActionBoton implements ActionListener{
+public class ActionBoton implements ActionListener {
 
-	VentanaPrincipal v = new VentanaPrincipal();
-	ControlJuego juego = new ControlJuego();
+	VentanaPrincipal v;
 	private int i;
 	private int j;
 	private boolean porExplosion;
+	private JButton aux = new JButton();
 
-	public ActionBoton() {
-		//TODO
+	public ActionBoton(VentanaPrincipal v) {
+		this.v = v;
+		
 	}
-	
+
 	/**
-	 *Acción que ocurrirá cuando pulsamos uno de los botones.
+	 * Acción que ocurrirá cuando pulsamos uno de los botones.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//TODO
-		for ( i = 0; i < v.botonesJuego.length; i++) {
-			for (j = 0; j < v.botonesJuego[i].length; j++) {
-				v.botonesJuego[i][j].addActionListener(new ActionListener(){
-					  int iLis = i;
-					  int jLis = j;
+		aux = (JButton) e.getSource();
 
-					@Override
-					public void actionPerformed(ActionEvent e){
-						if (juego.abrirCasilla(iLis, jLis)) {
-							v.mostrarNumMinasAlrededor(iLis, jLis);
-							porExplosion = false;
-							v.mostrarFinJuego(porExplosion);
-							v.refrescarPantalla();
-							v.actualizarPuntuacion();
+		for (i = 0; i < v.getJuego().LADO_TABLERO; i++) {
+			for (j = 0; j < v.getJuego().LADO_TABLERO; j++) {
+				if (aux == v.botonesJuego[i][j]) {
+					if (v.juego.abrirCasilla(i, j)) {
+						v.mostrarNumMinasAlrededor(i, j);
+						porExplosion = false;
+						v.mostrarFinJuego(porExplosion);
+						v.actualizarPuntuacion();
 
-
-						} else {
-							if(!juego.abrirCasilla(iLis, jLis)){
-								porExplosion = true;
-								v.mostrarFinJuego(porExplosion);
-							}
-							
-						}
+					} else {
+						porExplosion = true;
+						v.mostrarFinJuego(porExplosion);
 
 					}
-				});
-				
+				}
 			}
 		}
 	}
-
 }
